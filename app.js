5965c95ea93ac84c61e26380b4693b94d39f0d3e
Alvaro Lima <alvarolima093@gmail.com>
2025-03-18 20:54:30 -0300

    Adicionada funções de adicionar amigo e sortear

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+function adicionarAmigo() {
+    const campoInput = document.getElementById("amigo");
+    const nome = campoInput.value;
+
+    if (nome.trim() !== "") { 
+        const lista = document.getElementById("listaAmigos");
+        const item = document.createElement("li");
+        item.textContent = nome;
+        lista.appendChild(item);
+        campoInput.value = "";
+    } else {
+        alert("Por favor, insira um nome!");
+    }
+}
+
+function sortearAmigo() {
+    const lista = document.getElementById("listaAmigos");
+    const itens = lista.getElementsByTagName("li");
+
+    if (itens.length === 0) {
+        alert("Adicione amigos antes de sortear!");
+        return;
+    }
+
+    const indiceSorteado = Math.floor(Math.random() * itens.length);
+    const amigoSorteado = itens[indiceSorteado].textContent;
+    const resultado = document.getElementById("resultado");
+    const item = document.createElement("li");
+    item.textContent = `Amigo Sorteado: ${amigoSorteado}`;
+    resultado.appendChild(item);
+}
